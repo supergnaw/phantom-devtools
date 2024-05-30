1093504e2f83c2cf543fb6f4fad075579a5868d6
supergnaw <loren.supernaw@gmail.com>
2024-05-30 08:53:06 -0500

    Add files via upload

---
+++ encryption_helper.py
@@ -0,0 +1,18 @@
+def SecretStorageType(value, names=None, *, module=None, qualname=None, type=None, start=1) -> None:
+    return
+
+
+def decrypt(cipher: str, salt: str) -> str:
+    return ""
+
+
+def encrypt(plain: str, salt: str) -> str:
+    return ""
+
+
+def get_encryption_backend(secret_storage_type):
+    return
+
+
+class encryption_manager:
+    pass
